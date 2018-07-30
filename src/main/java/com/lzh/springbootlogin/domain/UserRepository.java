7cf6c0d10a29dfaf77e651c5a9ab79d2c1d84c12
Zhanghan <zhanghan.liu@incurator.com>
2018-07-30 17:17:58 -0500

    login implemented

--- src/main/java/com/lzh/springbootlogin/domain/UserRepository.java
+++ src/main/java/com/lzh/springbootlogin/domain/UserRepository.java
@@ -2,5 +2,8 @@ package com.lzh.springbootlogin.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    public List<User> findByUsername(String username);
 }
--- src/main/java/com/lzh/springbootlogin/web/LoginController.java
+++ src/main/java/com/lzh/springbootlogin/web/LoginController.java
@@ -3,7 +3,11 @@ package com.lzh.springbootlogin.web;
 import com.lzh.springbootlogin.domain.User;
 import com.lzh.springbootlogin.domain.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class LoginController {
@@ -19,16 +23,45 @@ public class LoginController {
 
     @CrossOrigin(origins = "http://localhost:4200")
     @PostMapping(value="/register")
-    public String registerPost(@RequestBody User user) {
+    public ResponseEntity registerPost(@RequestBody User user) {
         // System.out.println(user.toString());
-        user.setUsername(user.getUsername());
-        user.setPassword(user.getPassword());
-        userRepository.save(user);
-        return "redirect:/login";
+        try {
+            List<User> userList = userRepository.findByUsername(user.getUsername());
+
+            if (userList.isEmpty()) {
+                user.setUsername(user.getUsername());
+                user.setPassword(user.getPassword());
+                userRepository.save(user);
+                return new ResponseEntity(HttpStatus.OK);
+            } else {
+                return new ResponseEntity(HttpStatus.BAD_REQUEST);
+            }
+
+        } catch (Exception e) {
+           return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
     }
 
-    @GetMapping("/login")
-    public String loginPage() {
-        return "login";
+    @CrossOrigin(origins = "http://localhost:4200")
+    @PostMapping("/login")
+    public ResponseEntity loginPage(@RequestBody User user) {
+        try {
+            List<User> userList = userRepository.findByUsername(user.getUsername());
+
+            for (User foundUser: userList) {
+                if (user.getUsername().equals(foundUser.getUsername()) &&
+                        user.getUsername().equals(foundUser.getUsername())) {
+                    return new ResponseEntity(HttpStatus.OK);
+                } else {
+                    return new ResponseEntity(HttpStatus.BAD_REQUEST);
+                }
+            }
+
+        } catch (Exception e) {
+            return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        return null;
     }
 }
